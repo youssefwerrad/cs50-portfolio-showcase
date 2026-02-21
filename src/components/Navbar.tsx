1cf603dfbc5261b1685414c6eb529fb0ba09301a
youssef werrad <youssefwerrad@gmail.com>
2026-02-21 14:08:58 +0000

    Sat, Feb 21, 2026, 2:08 PM +00:00

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,6 @@ import { Menu, X } from "lucide-react";
 const links = [
   { label: "Skills", href: "#skills" },
   { label: "Technologies", href: "#technologies" },
-  { label: "Courses", href: "#courses" },
   { label: "Projects", href: "#projects" },
 ];
 
